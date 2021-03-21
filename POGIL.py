66b426867ebc370d89edd8cd2f71f83894a776b7
booyuck <81058127+booyuck@users.noreply.github.com>
2021-03-20 21:42:34 -0400

    Delete POGIL.py

--- POGIL.py
+++
@@ -1,8 +0,0 @@
-def absolute_value(num):
-    if num >= 0:
-        return num
-    else:
-        return -num
-print(absolute_value(int(input('number please'))))
-
-print(absolute_value(int(input('number please'))))
